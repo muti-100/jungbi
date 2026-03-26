91bad060cb57b4e9f7ac37bcdaacc4510a060d61
muti-100 <tjdckd01@gmail.com>
2026-03-26 21:34:36 +0900

    remove projects from git tracking

--- projects/waruru/src/backend/src/abtest/abtest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AbTestController } from './abtest.controller';
-import { AuthModule } from '../auth/auth.module';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [AbTestController],
-})
-export class AbTestModule {}
--- projects/waruru/src/backend/src/legacy/legacy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LegacyController } from './legacy.controller';
-import { AuthModule } from '../auth/auth.module';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [LegacyController],
-})
-export class LegacyModule {}
